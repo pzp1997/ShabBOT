57405ec363f2214f58f106d6a8f65bd7370c88a6
Palmer Paul <pzpaul2002@yahoo.com>
2017-03-06 14:20:49 -0500

    add tests for normalize frequency

--- tests/shabbot_test.py
+++ tests/shabbot_test.py
@@ -1,6 +1,23 @@
 from .context import shabbot
 
+import types
 import unittest
+
+
+class TestNormalizeFrequency(unittest.TestCase):
+
+    def test_returns_generator(self):
+        self.assertIs(types.GeneratorType, type(shabbot.normalize_freq([])))
+
+    def test_empty_list(self):
+        self.assertEqual([], list(shabbot.normalize_freq([])))
+
+    def test_uniform_probability(self):
+        self.assertEqual([0.2] * 5, list(shabbot.normalize_freq([1] * 5)))
+
+    def test_non_uniform_probability(self):
+        self.assertEqual([0.1, 0.6, 0.3],
+                         list(shabbot.normalize_freq([2, 12, 6])))
 
 
 if __name__ == '__main__':
