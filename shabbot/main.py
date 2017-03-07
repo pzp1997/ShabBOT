45276c01faa0f73141482d25a924ed3855fb6c14
Palmer Paul <pzpaul2002@yahoo.com>
2017-03-07 13:04:07 -0500

    use updated database_helper and email_helper, and small changes to main.py

--- shabbot/main.py
+++ shabbot/main.py
@@ -2,52 +2,49 @@
 
 """Helps coordinate Shabbat meals in the Orthodox Community at Penn (OCP)."""
 
+import itertools
+import operator
 import random
 
 from shabbot import database_helper
+from shabbot import email_helper
 
 __author__ = "Palmer Paul"
-__version__ = "1.0.0"
 __email__ = "pzpaul2002@yahoo.com"
-__status__ = "Development"
+__version__ = "1.0.0"
 
 NUM_MEALS = 1
-SIZE_OF_MEAL = 2
-MESSAGE = '''Hello!\n\n
+SIZE_OF_MEAL = 10
+
+MESSAGE = """Hello!\n\n
     I am the OCP Shab-bot. Each week, I automatically invite twelve random
     OCPeople to Shabbat Lunch. This week, my algorithms have smiled down
     upon you.\n\n
     Please reply-all with what you are bringing (someone/s claim the main),
     and arrange a place to eat. Unfortunately I will not be able to make
-    it, because I have SmarterChild\'s bot-mitzvah this weekend and also
+    it, because I have SmarterChild's bot-mitzvah this weekend and also
     because my digestive system is made of javascript.\n\n
-    This week\'s randomly generated meal theme is (U.S. season 7).\n\n
+    This week's randomly generated meal theme is (U.S. season 7).\n\n
     Enjoy, and let me know how this goes! Always looking to update my
-    programming and oil my joints.\n\n'''
+    programming and oil my joints.\n\n"""
 
 
-@database_helper.ocp_database_required
-def retrieve_student_emails(db_conn=None):
+def retrieve_student_emails(db_conn):
     """
     Retrieve email addresses of students from the MySQL database.
 
+    Args:
+        db_conn: an open MySQL database connection to retrieve the emails from
     Returns:
-        A list of email addresses of people in the OCP.
+        A list of one-tuples of email addresses of people in the OCP
     """
-    query = 'SELECT email FROM subscriber WHERE grad_year > 2016'
+    query = '''SELECT email FROM subscriber
+        WHERE grad_year > 2016 AND email IS NOT NULL'''
     cursor = db_conn.cursor()
     cursor.execute(query)
-    return list(cursor)
-
-
-def email_invitees(invitees):
-    """
-    Email the invited guests.
-
-    Args:
-        invitees: a list of emails of those who are invited to the meal
-    """
-    print list(invitees)
+    out = cursor.fetchall()
+    cursor.close()
+    return out
 
 
 def make_meals(emails):
@@ -57,24 +54,36 @@ def make_meals(emails):
     Args:
         emails: a list of email addresses of potential invitees
     Returns:
-        A list of the people who were invited to meals
+        A list of meals, where each meal is a tuple of email addresses
     """
-    invitees = random.sample(emails, SIZE_OF_MEAL * NUM_MEALS)
+    random_emails = random.sample(emails, SIZE_OF_MEAL * NUM_MEALS)
+    invitees = itertools.imap(operator.itemgetter(0), random_emails)
 
-    # Email invitees
-    for i in range(0, invitees, SIZE_OF_MEAL):
-        email_invitees(invitees[i:i+SIZE_OF_MEAL])
-
-    return invitees
+    meals = []
+    for _ in xrange(NUM_MEALS):
+        meal = tuple(itertools.islice(invitees, SIZE_OF_MEAL))
+        meals.append(meal)
+    return meals
 
 
 def main():
     """
     Entry point for the script.
     """
-    students = retrieve_student_emails()
+    db_conn = database_helper.connect_to_mysql(
+        database_helper.get_mysql_config())
 
-    invitees = make_meals(students)
+    students = retrieve_student_emails(db_conn)
+
+    meals = make_meals(students)
+
+    user, password = email_helper.get_email_config()
+    mail = email_helper.login(user, password)
+
+    for meal in meals:
+        email_helper.send_invite(mail, user, meal, MESSAGE)
+
+    mail.quit()
 
 
 if __name__ == '__main__':
