7dff23f960839fca3993ef800d567731ac5dfb11
Palmer Paul <pzpaul2002@yahoo.com>
2017-03-07 12:49:48 -0500

    simplify database_helper

--- shabbot/database_helper.py
+++ shabbot/database_helper.py
@@ -1,8 +1,8 @@
 import ConfigParser
-from functools import wraps
 import os.path
 
 import mysql.connector
+
 
 def get_mysql_config():
     """
@@ -10,7 +10,7 @@ def get_mysql_config():
     ShabBOT/config.cfg under the MYSQL section.
 
     Returns:
-        A dictionary of configuration settings and credentials for MySQL DB.
+        A dictionary of configuration settings and credentials for MySQL DB
     """
     config = ConfigParser.ConfigParser()
     config.read(os.path.abspath(os.path.join(
@@ -22,7 +22,7 @@ def get_mysql_config():
     return dict(conf)
 
 
-def connect_to_db(config):
+def connect_to_mysql(config):
     """
     Try to connect to MySQL database with given configuration settings.
 
@@ -34,21 +34,3 @@ def connect_to_db(config):
     except mysql.connector.Error as err:
         print err
         exit(1)
-
-
-def database_required(config):
-    """
-    Decorator to ensure that a function has an open database connection.
-    """
-    cnx = connect_to_db(config)
-    def decorator(func):
-        @wraps(func)
-        def inner(*args, **kwargs):
-            if not cnx.is_connected():
-                cnx = connect_to_db(config)
-            kwargs['db_conn'] = cnx
-            return func(*args, **kwargs)
-        return inner
-    return decorator
-
-ocp_database_required = database_required(get_mysql_config())
